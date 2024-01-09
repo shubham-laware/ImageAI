836ef7b2c7179f0216b5521a87808ef055d5fb23
Shubham Laware <lawareshubham647@gmail.com>
2024-01-09 11:55:20 +0530

    file updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import Navbar from "./components/navbar/Navbar";
 import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import CreateImage from "./pages/createImage/CreateImage";
